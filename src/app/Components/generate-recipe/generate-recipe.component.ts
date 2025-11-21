b0e326f8d97e901aeec004392401ede16dd38005
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-11-21 23:55:18 +0100

    first commit

---
+++ src/app/Components/generate-recipe/generate-recipe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-recipe',
+  imports: [],
+  templateUrl: './generate-recipe.component.html',
+  styleUrl: './generate-recipe.component.scss'
+})
+export class GenerateRecipeComponent {
+
+}
---
+++ src/app/Components/recipes-overview/recipes-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-overview',
+  imports: [],
+  templateUrl: './recipes-overview.component.html',
+  styleUrl: './recipes-overview.component.scss'
+})
+export class RecipesOverviewComponent {
+
+}
