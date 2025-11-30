19ecfcf527d9628374a3b71f47408cd3d63a878a
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2025-11-30 21:42:27 +0100

    added first routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './Components/hero/hero.component';
+import { GenerateRecipeComponent } from './Components/generate-recipe/generate-recipe.component';
+import { CookbookComponent } from './Components/cookbook/cookbook.component';
 
 export const routes: Routes = [
-  {path: '', component: HeroComponent}
+  {path: '', component: HeroComponent},
+  {path:'ingredients', component: GenerateRecipeComponent},
+  {path:"cookbook", component: CookbookComponent}
 ];
